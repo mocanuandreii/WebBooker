07e67a9314b1432aeac50496a5af5be1a1656820
mocanuandreii <andreialex47@gmail.com>
2020-08-08 11:12:30 +0300

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -23,14 +23,14 @@ db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
 //Models/tables
-db.company = require('..\\model\\company.model.js')(sequelize, Sequelize);
-db.user = require('..\\model\\user.model.js')(sequelize, Sequelize);
-db.account = require('..\\model\\account.model.js')(sequelize, Sequelize);
-db.driverStatus = require('..\\model\\driverStatus.model.js')(sequelize, Sequelize);
-db.voucher = require('..\\model\\voucher.model.js')(sequelize, Sequelize);
-db.carType = require('..\\model\\cartype.model.js')(sequelize, Sequelize);
-db.price = require('..\\model\\price.model.js')(sequelize, Sequelize);
-db.booking = require('..\\model\\booking.model.js')(sequelize, Sequelize);
+db.company = require('../model/company.model.js')(sequelize, Sequelize);
+db.user = require('../model/user.model.js')(sequelize, Sequelize);
+db.account = require('../model/account.model.js')(sequelize, Sequelize);
+db.driverStatus = require('../model/driverStatus.model.js')(sequelize, Sequelize);
+db.voucher = require('../model/voucher.model.js')(sequelize, Sequelize);
+db.carType = require('../model/cartype.model.js')(sequelize, Sequelize);
+db.price = require('../model/price.model.js')(sequelize, Sequelize);
+db.booking = require('../model/booking.model.js')(sequelize, Sequelize);
 
 
 module.exports = db;
