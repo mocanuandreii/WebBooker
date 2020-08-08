583fd123d4dc37fa395788c1167b751308c0231d
mocanuandreii <andreialex47@gmail.com>
2020-08-08 11:25:22 +0300

    Update web-server.js

--- scripts/web-server.js
+++ scripts/web-server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static( rootPath + '/app'));
 
-const db = require('../app/config/db.config.js');
+const db = require('app/config/db.config.js');
 
 require('../app/controller/company.route.js')(app);
 require('../app/controller/user.route.js')(app);
